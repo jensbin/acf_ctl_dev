10188b79adb4bfadf8af87e90ad6caa75cbc5955
Torsten Boettjer <torsten.boettjer@avaloq.com>
2023-07-31 16:28:39 +0200

    provider changes

--- main.tf
+++ main.tf
@@ -3,100 +3,159 @@
 
 // readme.md created with https://terraform-docs.io/: terraform-docs markdown --sort=false ./ > ./readme.md
 
-// --- provider settings --- //
-terraform {
-  required_providers {
-    oci = {
-      source = "hashicorp/oci"
-    }
-  }
-}
-// --- provider settings  --- //
+// --- settings --- //
+variable "tenancy_ocid" {}
+variable "region" {}
+variable "compartment_ocid" {}
+variable "current_user_ocid" {}
+// --- settings --- //
 
-provider "oci" {
-  alias  = "service"
-  region = var.region
-}
-
-provider "oci" {
-  alias  = "home"
-  region = module.configuration.tenancy.region.key
-}
-
-variable "tenancy_ocid" { }
-
+// --- schema --- //
 locals {
-  topologies = flatten(compact([var.host == true ? "host" : "", var.nodes == true ? "nodes" : "", var.container == true ? "container" : ""]))
-  domains    = jsondecode(file("${path.module}/parameter/resident/domains.json"))
-  segments   = jsondecode(file("${path.module}/parameter/network/segments.json"))
+  topologies = flatten(compact([
+    var.management == true ? "management" : "", 
+    var.host == true ? "host" : "", 
+    var.nodes == true ? "nodes" : "", 
+    var.container == true ? "container" : ""
+  ]))
+  /*
+  domains    = jsondecode(file("${path.module}/default/resident/domains.json"))
+  segments   = jsondecode(file("${path.module}/default/network/segments.json"))
+  wallets    = jsondecode(file("${path.module}/default/encryption/wallets.json"))
+  */
 }
+// --- schema --- //
 
-// --- tenancy configuration --- //
+/*/ --- configuration --- //
 module "configuration" {
-  source         = "./config/"
+  source         = "./default/"
   providers = {oci = oci.service}
-  oci = {
-    tenancy      = var.tenancy_ocid
-    class        = var.class
-    owner        = var.owner
-    organization = var.organization
-    solution     = var.solution
-    repository   = var.repository
-    stage        = var.stage
-    region       = var.region
-    osn          = var.osn
+  account = {
+    tenancy_id     = var.tenancy_ocid
+    compartment_id = var.compartment_ocid
+    home           = var.region
+    user_id        = var.current_user_ocid
   }
-  resolve = {
+  resident = {
     topologies = local.topologies
     domains    = local.domains
     segments   = local.segments
   }
+  solution = {
+    adb          = "${var.adb_type}_${var.adb_size}"
+    budget       = var.budget
+    class        = var.class
+    encrypt      = var.create_wallet
+    name         = var.name
+    region       = var.location
+    organization = var.organization
+    osn          = var.osn
+    owner        = var.owner
+    repository   = var.repository
+    stage        = var.stage
+    tenancy      = var.tenancy_ocid
+    wallet       = var.wallet
+  }
 }
-// --- tenancy configuration  --- //
+// --- configuration --- /*/
 
 /*/ --- operation controls --- //
 module "resident" {
-  source = "github.com/avaloqcloud/terraform-oci-ocloud-asset-resident"
+  source     = "./assets/resident"
   depends_on = [module.configuration]
-  providers = {oci = oci.home}
-  tenancy   = module.configuration.tenancy
-  resident  = module.configuration.resident
-  input = {
-    # Reference to the deployment root. The service is setup in an encapsulating child compartment 
-    parent_id     = var.parent
+  providers  = {oci = oci.home}
+  schema = {
     # Enable compartment delete on destroy. If true, compartment will be deleted when `terraform destroy` is executed; If false, compartment will not be deleted on `terraform destroy` execution
-    enable_delete = alltrue([var.stage != "PROD" ? true : false, var.amend])
+    enable_delete = var.stage != "PRODUCTION" ? true : false
+    # Reference to the deployment root. The service is setup in an encapsulating child compartment 
+    parent_id     = var.tenancy_ocid
+    user_id       = var.current_user_ocid
+  }
+  config = {
+    tenancy = module.configuration.tenancy
+    service = module.configuration.service
   }
 }
 output "resident" {
-  value = {
-    for resource, parameter in module.resident : resource => parameter
+  value = {for resource, parameter in module.resident : resource => parameter}
+}
+// --- operation controls --- //
+
+// --- wallet configuration --- //
+module "encryption" {
+  source     = "./assets/encryption"
+  depends_on = [module.configuration, module.resident]
+  providers  = {oci = oci.service}
+  for_each   = {for wallet in local.wallets : wallet.name => wallet}
+  schema = {
+    create = var.create_wallet
+    type   = var.wallet == "SOFTWARE" ? "DEFAULT" : "VIRTUAL_PRIVATE"
+  }
+  config = {
+    tenancy    = module.configuration.tenancy
+    service    = module.configuration.service
+    encryption = module.configuration.encryption[each.key]
+  }
+  assets = {
+    resident   = module.resident
   }
 }
-// --- operation controls --- /*/
+output "encryption" {
+  value = {for resource, parameter in module.encryption : resource => parameter}
+  sensitive = true
+}
+// --- wallet configuration --- //
 
-/*/ --- network configuration --- //
+// --- network configuration --- //
 module "network" {
-  source = "github.com/avaloqcloud/terraform-oci-ocloud-asset-network"
-  depends_on = [module.configuration, module.resident]
+  source     = "./assets/network"
+  depends_on = [module.configuration, module.encryption, module.resident]
   providers = {oci = oci.service}
   for_each  = {for segment in local.segments : segment.name => segment}
-  tenancy   = module.configuration.tenancy
-  resident  = module.configuration.resident
-  network   = module.configuration.network[each.key]
-  input = {
-    internet = var.internet
-    nat      = var.nat
+  schema = {
+    internet = var.internet == "PUBLIC" ? "ENABLE" : "DISABLE"
+    nat      = var.nat == true ? "ENABLE" : "DISABLE"
     ipv6     = var.ipv6
     osn      = var.osn
   }
+  config = {
+    tenancy = module.configuration.tenancy
+    service = module.configuration.service
+    network = module.configuration.network[each.key]
+  }
   assets = {
-    resident = module.resident
+    encryption = module.encryption["main"]
+    resident   = module.resident
   }
 }
 output "network" {
-  value = {
-    for resource, parameter in module.network : resource => parameter
-    }
+  value = {for resource, parameter in module.network : resource => parameter}
 }
-// --- network configuration --- /*/
+// --- network configuration --- //
+
+// --- database creation --- //
+module "database" {
+  source     = "./assets/database"
+  depends_on = [module.configuration, module.resident, module.network, module.encryption]
+  providers  = {oci = oci.service}
+  schema = {
+    class    = var.class
+    create   = var.create_adb
+    password = var.create_wallet == false ? "RANDOM" : "VAULT"
+  }
+  config = {
+    tenancy  = module.configuration.tenancy
+    service  = module.configuration.service
+    database = module.configuration.database
+  }
+  assets = {
+    encryption = module.encryption["main"]
+    network    = module.network["core"]
+    resident   = module.resident
+  }
+}
+output "database" {
+  value = {for resource, parameter in module.database : resource => parameter}
+  sensitive = true
+}
+// --- database creation --- /*/
---
+++ provider.tf
@@ -0,0 +1,26 @@
+// Copyright (c) 2023 Avaloq and/or its affiliates.
+// Licensed under the Apache 2.0 license shown at https://www.apache.org/licenses/LICENSE-2.0.
+
+// readme.md created with https://terraform-docs.io/: terraform-docs markdown --sort=false ./ > ./readme.md
+
+terraform {
+  required_providers {
+    oci = {
+      source = "hashicorp/oci"
+    }
+    provider "google" {
+      project = "avc-dev"
+      region  = "us-central1"
+    }
+  }
+}
+
+provider "oci" {
+  alias  = "home"
+  region = module.configuration.tenancy.region.key
+}
+
+provider "oci" {
+  alias  = "service"
+  region = var.location
+}
