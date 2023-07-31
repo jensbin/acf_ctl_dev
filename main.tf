16848fe7129f93da64ef0827feb4544d750645a5
Torsten Boettjer <57480261+torstenboettjer@users.noreply.github.com>
2023-07-31 21:25:15 +0200

    Troubleshoot schema file

--- main.tf
+++ main.tf
@@ -3,9 +3,8 @@
 
 // readme.md created with https://terraform-docs.io/: terraform-docs markdown --sort=false ./ > ./readme.md
 
-// --- settings --- //
+/*/ --- settings --- //
 variable "tenancy_ocid" {}
-/*
 variable "region" {}
 variable "compartment_ocid" {}
 variable "current_user_ocid" {}
