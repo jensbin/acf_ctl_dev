518b7038de0496ab9754725cdc5a707d84e3a21d
Torsten Boettjer <torsten.boettjer@avaloq.com>
2023-08-01 21:08:28 +0200

    new logo 130

--- schema.tf
+++ schema.tf
@@ -38,64 +38,52 @@ variable "own" {
   description = "The technical owner identifies the main administrator by his or her eMail address"
 }
 
-variable "stage"           { 
+variable "stg"           { 
   type = string
   description = "The stage variable triggers lifecycle related resources to be provisioned"
-  validation {
-    condition     = length(regexall("^[A-Za-z][A-Za-z0-9]{1,3}$", var.stage)) > 0
-    error_message = "A service_name variable is required and must contain alphanumeric characters only, start with a letter and 4 character max."
-  }
 }
 
-variable "lcal" {
+variable "lcl" {
   type        = string
   description = "The locations triggers the target region for a service deployment"
-  default     = "eu-zurich-1"
 }
 
 # Components
 variable "foundation" {
   type        = bool
   description = "Deploying the ACP foundation, incl. AMI and BDE"
-  default     = true
 }
 
 variable "smart" {
   type        = bool
   description = "Deploying SmartClient together with the integration server"
-  default     = true
 }
 
 variable "capi" {
   type        = bool
   description = "Deploying the community API"
-  default     = true
 }
 
 # Domain Protection
 variable "protect" {
   type        = bool
   description = "The flag enables operators to delete compartments with terraform destroy. For production deployments, this flag should be disabled."
-  default     = true
 }
 
 # Network Settings
-variable "internet" {
+variable "inet" {
   type        = bool
   description = "Allows or disallows to provision resources with public IP addresses."
-  default     = true
 }
 
 variable "nat" {
   type        = bool
   description = "Enables or disables routes through a NAT Gateway."
-  default     = true
 }
 
 variable "ipv6" {
   type        = bool
   description = "Triggers the release of IPv6 addresses inside the VCN."
-  default     = false
 }
 
 variable "osn" {
